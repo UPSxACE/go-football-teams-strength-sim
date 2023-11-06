949e1712996ccde80febe998bdf64b17ba975229
Eduardo Botelho <eduardo11224b@gmail.com>
2023-11-06 14:51:04 +0000

    added exceptions to teams.Create

--- teams/create.go
+++ teams/create.go
@@ -1,9 +1,28 @@
 package teams
 
-func Create(name string, strength int) Team{
+import (
+	"errors"
+	"regexp"
+)
+
+func Create(name string, strength int) (Team, error){
+	if(name == ""){
+		return Team{}, errors.New("teams.Create: empty name")
+	}
+
+	if(len(name) > 50){
+		return Team{}, errors.New("teams.Create: name is bigger than 50 characters")
+	}
+
+	onlyLettersNumbersSpaces := regexp.MustCompile("^[a-zA-Z0-9\\s]*$")
+
+	if(!onlyLettersNumbersSpaces.MatchString(name)){
+		return Team{}, errors.New("teams.Create: invalid characters in name")
+	}	
+
 	teamId := index;
 	index++;
 	newTeam := Team{teamId,name,strength}
 	listOfTeams = append(listOfTeams, newTeam)
-	return newTeam
+	return newTeam, nil
 }
