975fff18592cce59bd24c11f56ad29aed3f31395
Eduardo Botelho <eduardo11224b@gmail.com>
2023-11-08 23:03:44 +0000

    updated game.go: the app logic is now being run with while loops

--- game.go
+++ game.go
@@ -5,41 +5,71 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"strings"
+	"strconv"
 
 	"github.com/UPSxACE/go-football-teams-strength-sim/teams"
+	"github.com/UPSxACE/go-football-teams-strength-sim/utils"
 )
 
-func ask(reader *bufio.Reader, question string) (string, error){
-	fmt.Print(question + " ")
 
-	input, err := reader.ReadString('\n')
-		
-	// convert CRLF to LF
-	input = strings.Replace(input, "\n", "", -1)
 
-	return strings.Replace(input, "\r", "", -1), err
-}
 
 func main(){
-	fmt.Println("Hello world")
-	teams.Create("Real Madrid", 90)
-	teams.Create("Barcelona", 90)
-	fmt.Println(teams.List())
-
+	
+	
 	
 	reader := bufio.NewReader(os.Stdin)
 	selectedOption := ""
 	
-	for selectedOption != "q" {
-		input, err := ask(reader, "Type something:")
+	fmt.Println(utils.LineMessage("WELCOME TO FOOTBALL SIMULATOR"))
+	fmt.Println("We will start by adding teams, and then we will simulate the outcome of a tournament with those teams.")
+	
+	utils.PressAnyKey()
+	utils.Clear()
 
-		if(err != nil){
+	for (selectedOption != "n" && selectedOption != "N") {
+		selectedOption = "";
+
+		teamName, err := utils.Ask(reader, "Name of the team:")
+
+		if err != nil {
 			log.Fatalf("Unexpected error: %v", err)
 		}
 
-		selectedOption = input
+		teamStrength, err := utils.Ask(reader, "Strength of the team:")
 
-		fmt.Println("You typed: ", selectedOption)
+		if err != nil {
+			log.Fatalf("Unexpected error: %v", err)
+		}
+
+		teamStrengthInt, err := strconv.Atoi(teamStrength)
+
+		if err != nil {
+			fmt.Println("Invalid input on team name or strength.")
+			err = nil
+		} else {
+			_, err = teams.Create(teamName, teamStrengthInt)
+
+			if err != nil {
+				fmt.Println("Invalid input on team name or strength.")
+				err = nil
+			}
+		}	
+
+		if len(teams.List()) >= 2 {
+			for selectedOption != "n" && selectedOption != "N" && selectedOption != "y" && selectedOption != "Y" {
+				answer, err := utils.Ask(reader, "Do you want to create another team? (y/n):")
+
+				if err != nil {
+					log.Fatalf("Unexpected error: %v", err)
+				}
+	
+				selectedOption = answer
+			}	
+		}
 	}
+
+	utils.Clear()
+	fmt.Println(teams.List())
+	fmt.Println(utils.LineMessage("THANK YOU!"))
 }
